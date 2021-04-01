673dfe7fbbfdee2e3133a4335fed6a268e2d239d
Valeria Chub <valeriachub.dev@gmail.com>
2021-04-01 23:07:06 +0300

    first commit

--- TableApp/CustomCell.swift
+++ TableApp/CustomCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class CustomCell: UITableViewCell {
-
+    
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var cellText: UITextView!
     @IBOutlet weak var cellLabel: UILabel!
@@ -9,8 +9,26 @@ class CustomCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    
+    private var observation: NSKeyValueObservation?
+    @objc var updateValue: UpdateValue!
+    
+    func configure(updateValue: UpdateValue, image: UIImage?) {
+        
+        print("configure")
+        
+        cellImage.image = image
+        self.updateValue = updateValue
+        observation = self.updateValue.observe(\.count, options: .new) { updateValue, change in
+            
+            self.cellText.text = "\(updateValue.count)"
+            
+            print("observation")
+        }
+    }
+    
 }
--- TableApp/ViewController.swift
+++ TableApp/ViewController.swift
@@ -1,6 +1,17 @@
 import UIKit
 
+class UpdateValue: NSObject {
+    
+    @objc dynamic var count: Int = 0
+}
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var timer: Timer?
+    var updateValue = UpdateValue()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return DATA.count
@@ -9,9 +20,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
         
-        cell.cellImage.image = UIImage(named: String(indexPath.row))
-        cell.cellText.text = DATA[indexPath.row].quote
-        cell.cellLabel.text = DATA[indexPath.row].author
+        cell.configure(updateValue: updateValue, image: UIImage(named: String(indexPath.row)))
+        print("cellForRowAt")
         
         return cell
     }
@@ -19,5 +29,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timer =  Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateTimer),
+                                      userInfo: nil, repeats: true)
+    }
+    
+    @objc
+    private func updateTimer() {
+        
+        updateValue.count += 1
+        
     }
 }
